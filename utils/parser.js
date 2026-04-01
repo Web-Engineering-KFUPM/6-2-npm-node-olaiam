766a073a94f888dffb2341159384b1967bf590dd
olaiam <s202283060@kfupm.edu.sa>
2026-04-01 11:17:05 +0300

    TODO6

--- utils/parser.js
+++ utils/parser.js
@@ -17,3 +17,11 @@ export function add(numbers) {
 export function subtract(numbers) {
   return numbers.slice(1).reduce((result, num) => result - num, numbers[0]);
 }
+
+export function multiply(numbers) {
+  return numbers.reduce((result, num) => result * num, 1);
+}
+
+export function divide(numbers) {
+  return numbers.slice(1).reduce((result, num) => result / num, numbers[0]);
+}
